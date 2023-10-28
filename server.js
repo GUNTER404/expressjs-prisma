5b5554446f9bfea3e599b19489dd62605bff525f
GUNTER404 <109247531+GUNTER404@users.noreply.github.com>
2023-10-28 18:20:32 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,40 @@
+const express = require('express');
+const { exec } = require('child_process');
 const http = require('http');
+const socketIO = require('socket.io');
 
-const PORT = process.env.PORT || 3000;
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('bruh');
+// Socket.IO event handler
+io.on('connection', (socket) => {
+  console.log('A client connected.');
+
+  // Event listener for 'runCommand' event
+  socket.on('runCommand', (command) => {
+    console.log('Received command:', command);
+
+    // Run the command in the background
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error executing command: ${error.message}`);
+        socket.emit('commandOutput', `Error executing command: ${error.message}`);
+      } else {
+        console.log('Command output:', stdout);
+        socket.emit('commandOutput', stdout);
+      }
+    });
+  });
+
+  // Event listener for 'disconnect' event
+  socket.on('disconnect', () => {
+    console.log('A client disconnected.');
+  });
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Start the server
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
