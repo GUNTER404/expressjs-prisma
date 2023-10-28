f113e4a0535e0894d8dff4faa1f210945daf07aa
GUNTER404 <109247531+GUNTER404@users.noreply.github.com>
2023-10-28 18:46:30 +0100

    Create stop.sh

---
+++ stop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set your ngrok authentication token
+authtoken="2XOzDJSZPPLzR0jUidnxnP8yjEo_6XsmGdb6WDLpVJSBvp3yK"
+
+# Get the list of ngrok tunnels using the API
+tunnels=$(curl -s -H "Authorization: Bearer $authtoken" https://api.ngrok.com/api/tunnel_sessions)
+
+# Extract the first tunnel ID
+tunnel_id=$(echo "$tunnels" | jq -r '.tunnel_sessions[0].id' | awk -F'/' '{print $NF}')
+
+# Stop the first tunnel using the API
+curl -X DELETE -H "Authorization: Bearer $authtoken" "https://api.ngrok.com/api/tunnels/$tunnel_id"
